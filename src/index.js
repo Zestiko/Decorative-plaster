98b73d187e7c14cd70a6fe8a07019f1ca6f115da
Alieksieiev Vladislav <39629598+Zestiko@users.noreply.github.com>
2022-11-02 13:53:21 +0200

    Add markup

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css'
+
+let galleryNew = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
