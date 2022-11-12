a42e0390f1a9c69c8c60c920561cc146849ce2cb
Alejksejejv Vladislav <39629598+Zestiko@users.noreply.github.com>
2022-11-12 21:37:53 +0200

    Add some styles

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
+import Glide from "@glidejs/glide";
+import "@glidejs/glide/src/assets/sass/glide.core.scss";
 import SimpleLightbox from "simplelightbox";
-import 'simplelightbox/dist/simple-lightbox.min.css'
+import 'simplelightbox/dist/simple-lightbox.min.css';
+const refs = {
+    menuButton: document.querySelector('[data-menu-button]'),
+    mobileMenu: document.querySelector('[data-menu]')
+};
+
+refs.menuButton.addEventListener('click', () => {
+    refs.menuButton.classList.toggle("is-active");
+    refs.mobileMenu.classList.toggle("is-open");
+});
+
 
 let galleryNew = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
+new Glide('.glide').mount();
