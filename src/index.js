16983391d9d22fb1efc7bfe1c0224afdfc523075
Alejksejejv Vladislav <39629598+Zestiko@users.noreply.github.com>
2022-11-16 19:51:01 +0200

    fix bugs

--- src/index.js
+++ src/index.js
@@ -6,20 +6,36 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 import SmoothScroll from "smooth-scroll";
 const refs = {
     menuButton: document.querySelector('[data-menu-button]'),
-    mobileMenu: document.querySelector('[data-menu]')
+    mobileMenu: document.querySelector('[data-menu]'),
+    nav: document.querySelector('.nav__link')
 };
-
+console.log(refs.nav)
 refs.menuButton.addEventListener('click', () => {
     refs.menuButton.classList.toggle("is-active");
-    refs.mobileMenu.classList.toggle("is-open");
+  refs.mobileMenu.classList.toggle("is-open");
+
 });
+// refs.nav.addEventListener('click', () => {
+//     refs.menuButton.classList.toggle("is-active");
+//     refs.mobileMenu.classList.toggle("is-open");
+//   // if (window.innerWidth <= 767) {
+    
+//   // }
+// })
+// Close the mobile menu on wider screens if the device orientation changes
+  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+    if (!e.matches) return;
+    mobileMenu.classList.remove('is-open');
+    refs.mobileMenu.setAttribute('aria-expanded', false);
+    bodyScrollLock.enableBodyScroll(document.body);
+  });
 
 
 let galleryNew = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
 const checkbox = document.querySelector('.glide')
 const glide = new Glide('.glide', {
   hoverpause: true,
-  // autoplay: 2000,
+  autoplay: 2000,
     perView: 4,
   breakpoints: {
     1024: {
